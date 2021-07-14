d8dcfccdce225aac28b4bcf4c4bbc8d6b31a5b58
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-14 22:30:41 +0530

    Update Prime_Number.java

--- July/14 July/Prime_Number.java
+++ July/14 July/Prime_Number.java
@@ -36,28 +36,3 @@ public class Prime_Number {
 }
 
 
-//import java.util.*;
-//
-//public class Prime_Number {
-//
-//  public static void main(String[] args) {
-//    Scanner scn = new Scanner(System.in);
-//
-//    int t = scn.nextInt();
-//    for (int i = 0; i < t; i++) {
-//      int n = scn.nextInt();
-//      int count = 0;
-//      for (int div = 1; div <= n; div++) {
-//        if (n % div == 0) {
-//          count++;
-//        }
-//      }
-//      if (count == 2) {
-//        System.out.println("prime");
-//      } else {
-//        System.out.println("notprime");
-//      }
-//    }
-//
-//  }
-//}
