44154e2a152a04e6e57fd31d48cdd360855936d7
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-14 22:30:16 +0530

    Update Prime_Number.java

--- July/14 July/Prime_Number.java
+++ July/14 July/Prime_Number.java
@@ -1,3 +1,11 @@
+/*                                        Question 
+ou've to check whether a given number is prime or not.
+ Take a number "t" as input representing count of input numbers to be tested.
+ Take a number "n" as input "t" number of times.
+ For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
+ */
+
+
 package July.July14;
 import java.util.Scanner;
 public class Prime_Number {
