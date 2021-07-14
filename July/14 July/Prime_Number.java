8824459d57a7f8bf8fdc9ba11e0a401e4363b833
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-14 22:31:47 +0530

    Update Prime_Number.java

--- July/14 July/Prime_Number.java
+++ July/14 July/Prime_Number.java
@@ -1,5 +1,5 @@
 /*                                        Question 
-ou've to check whether a given number is prime or not.
+you've to check whether a given number is prime or not.
  Take a number "t" as input representing count of input numbers to be tested.
  Take a number "n" as input "t" number of times.
  For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
