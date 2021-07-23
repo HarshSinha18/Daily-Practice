f220a01dbbb4776f7c8f15298f14ae725a0eabd4
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-23 21:58:04 +0530

    Update BigSum.java

--- July/23 July/BigSum.java
+++ July/23 July/BigSum.java
@@ -1,3 +1,11 @@
+/* Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+            5
+1000000001 1000000002 1000000003 1000000004 1000000005
+Output--> 5000000015
+
+*/
+
+
 package July.July14;
 import java.util.Scanner;
 public class BigSum {
