7e75d0dff5cd210d93f53294e853fb0fbfb2bfbb
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-23 21:32:25 +0530

    Update CompareTheTriplets.java

--- July/23 July/CompareTheTriplets.java
+++ July/23 July/CompareTheTriplets.java
@@ -1,3 +1,15 @@
+/*The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+
+ [ 1 , 2 , 3 ]
+ [ 3 , 2 , 1 ] , Output-> 1 1 
+ 	
+*/	
+
+
 package July.July14;
 import java.util.Scanner;
 public class CompareTheTriplets {
