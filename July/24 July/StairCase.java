19de32d9e04bddd9b5bacb39179e89a453882506
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-26 10:00:08 +0530

    Add files via upload

---
+++ July/24 July/StairCase.java
@@ -0,0 +1,25 @@
+package July.July14;
+import java.util.Scanner;
+public class StairCase {
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 int n = sc.nextInt();
+	 staircase(n);
+	}
+	public static void staircase(int n) {
+		int space = n-1;
+		for(int i = 1 ; i <= n ; i++) {
+			// Space Loop
+			for(int j = 1 ; j <= space ; j++) {
+				System.out.print(" ");
+		}
+			space--;
+			// Value Loop
+			for(int j = 1 ; j<= i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+				
+	}
+	}
+}
