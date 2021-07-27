3e144a13de19e7136ba5f956cff3ce38c418192c
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-07-27 08:12:15 +0530

    Update GradingStudents.java

--- July/26 July/GradingStudents.java
+++ July/26 July/GradingStudents.java
@@ -21,7 +21,6 @@ public class GradingStudents {
 	public static void gradingStudents(int grades[] , int n) {
 		int sum = 0;
 		int ans = 0;
-		System.out.println("Output");
 		for(int j = 0 ; j < n ; j++) {
 			for(int k = 0 ; k< 5 ; k++) {
 				 sum = grades[j]+k;
@@ -36,10 +35,7 @@ public class GradingStudents {
 					break;
 				}
 			}
-		}
-			
-			
-		}
-		
+		}		
+		}	
 }
 }
