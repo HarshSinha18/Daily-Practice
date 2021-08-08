0bc38298546f7a303d965e6d800e48c8e5c90bf4
Harsh Sinha <harshsinha.sd.18@gmail.com>
2021-08-08 18:34:05 +0530

    Update and rename DreawingBook.java to DrawingBook.java

--- July/28 July/DrawingBook.java
+++ July/28 July/DrawingBook.java
@@ -1,6 +1,6 @@
 package July.July14;
 import java.util.Scanner;
-public class DreawingBook {
+public class DrawingBook {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
